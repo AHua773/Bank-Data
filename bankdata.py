9a98601c4cf05c6518f72ce642046c4fd47e8004
AHua773 <zhhhuahua2002@163.com>
2022-11-09 13:39:00 +0800

    11

--- bankdata.py
+++ bankdata.py
@@ -9,6 +9,8 @@ st.write(data)
 st.markdown('**Number of Instances: 41188** ') 
 st.markdown('**Number of Attributes: 20 + output attribute.**')
 
+
+
 fig, ax = plt.subplots(2,1)
 y_count=data.y.value_counts()
 data.y.hist(ax=ax[0],figsize=(6,5))
@@ -18,8 +20,65 @@ sizes = data['y'].value_counts(sort = True)
 plt.pie(sizes,labels=labels,autopct='%1.1f%%', shadow=True, startangle=270,)
 plt.title('Yes and No categ',size = 12)
 st.pyplot(fig)
+st.markdown(' we saw that out of **more than 44,000** people,**less than 5,000** people accepted the business, and the success rate was 11.3%')
+st.write('')
+st.write('')
 
+st.markdown('**These graphs are made to conducted a separate analysis of each influencing factor, discussing the impact of its variables on success**')
+fig,ax= plt.subplots(6,1,figsize=(10,60))
+temp_1 = pd.DataFrame()
+temp_1['No_deposit'] = data[data['y'] == 'no']['job'].value_counts()
+temp_1['Yes_deposit'] = data[data['y'] == 'yes']['job'].value_counts()    
+ax[0].set_ylabel('Number of clients')
+ax[0].set_title('Distribution of {} and deposit'.format('job'))
+temp_1.plot.bar(ax=ax[0])
 
+temp_2 = pd.DataFrame()
+temp_2['No_deposit'] = data[data['y'] == 'no']['marital'].value_counts()
+temp_2['Yes_deposit'] = data[data['y'] == 'yes']['marital'].value_counts()    
+plt.ylabel('Number of clients')
+ax[1].set_title('Distribution of {} and deposit'.format('marital'))
+temp_2.plot.bar(ax=ax[1])
+
+temp_3 = pd.DataFrame()
+temp_3['No_deposit'] = data[data['y'] == 'no']['education'].value_counts()
+temp_3['Yes_deposit'] = data[data['y'] == 'yes']['education'].value_counts()    
+plt.ylabel('Number of clients')
+ax[2].set_title('Distribution of {} and deposit'.format('education'))
+temp_3.plot.bar(ax=ax[2])
+
+temp_4 = pd.DataFrame()
+temp_4['No_deposit'] = data[data['y'] == 'no']['contact'].value_counts()
+temp_4['Yes_deposit'] = data[data['y'] == 'yes']['contact'].value_counts()    
+plt.ylabel('Number of clients')
+ax[3].set_title('Distribution of {} and deposit'.format('contact'))
+temp_4.plot.bar(ax=ax[3])
+
+temp_5 = pd.DataFrame()
+temp_5['No_deposit'] = data[data['y'] == 'no']['loan'].value_counts()
+temp_5['Yes_deposit'] = data[data['y'] == 'yes']['loan'].value_counts()    
+ax[4].set_ylabel('Number of clients')
+ax[4].set_title('Distribution of {} and deposit'.format('loan'))
+temp_5.plot.bar(ax=ax[4])
+
+temp_6= pd.DataFrame()
+temp_6['No_deposit'] = data[data['y'] == 'no']['housing'].value_counts()
+temp_6['Yes_deposit'] = data[data['y'] == 'yes']['housing'].value_counts()    
+ax[5].set_ylabel('Number of clients')
+ax[5].set_title('Distribution of {} and deposit'.format('housing'))
+temp_6.plot.bar(ax=ax[5])
+st.pyplot(fig)
+
+from PIL import Image
+image = Image.open('dataimage1.png')
+st.markdown('**The graph to study the interaction between the various variables**')
+st.image(image, caption='dataimage1.png',use_column_width=True)
+st.write('')
+st.write('')
+
+st.markdown('The education level of customers and CPI are the same as them; At the same time, we found that these two groups of variables can influence each other, so we want to know:')
+st.markdown('**The first question is that the number of people who in different occupations will agree to deposit money on different day of a week. And then exchange.**')
+st.markdown('**The second question is that the number of people who will agree or disagree to deposit money in different CPI when they are in different considerably by education. And then exchange.**')
 job_label= st.sidebar.radio(
      'Select job',
      ('management','services','admin.','blue-collar','entrepreneur','housemaid','retired','self-employed','student','technician','unemployed','unknown'))  
@@ -54,6 +113,7 @@ for o in data.education:
     else:
         pass
 data_forms=data[data.education==p][data.day_of_week==n][data.job == l]
+st.header('')
 st.write(data_forms)
 
 for b in data.job: 
